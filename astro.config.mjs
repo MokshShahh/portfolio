afea259c9fd74a4f21e209d2ea62c09fb51a4f60
Moksh <moksh13112006@gmail.com>
2026-07-29 08:58:14 +0530

    feat: AI seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: "https://mokshshah.tech",
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
